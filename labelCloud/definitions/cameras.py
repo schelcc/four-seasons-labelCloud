8723d86f13130b2b334561d67ae5e115a16a3ed9
Cole Scheller <schelcc@pobox.com>
2024-02-10 18:51:56 -0500

    more verbose docstring

--- labelCloud/definitions/cameras.py
+++ labelCloud/definitions/cameras.py
@@ -1,7 +1,10 @@
 from enum import IntEnum
 
 class Camera(IntEnum):
-    """Which camera a 2d point corresponds to"""
+    """Which camera a 2d point corresponds to.
+    Left : 0
+    Middle : 1
+    Right : 2"""
     
     LEFT = 0
     MIDDLE = 1
