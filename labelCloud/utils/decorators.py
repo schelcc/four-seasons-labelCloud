7f73a60047b73f95fa249c70413af5612d34859a
Cole Scheller <schelcc@pobox.com>
2024-02-10 23:49:40 -0500

    utility decorators for usage mode restrictions

---
+++ labelCloud/utils/decorators.py
@@ -0,0 +1,27 @@
+from functools import wraps
+
+from ..control.config_manager import config
+
+def in_labeling_only_decorator(func):
+    """
+    Only execute function in labeling usage mode
+    """
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if config.get("FILE", "usage_mode") == "label":
+            return func(*args, **kwargs)
+        
+    return wrapper
+
+def in_projection_only_decorator(func):
+    """ 
+    Only execute in projection usage mode
+    """
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if config.get("FILE", "usage_mode") == "projection":
+            return func(**args, **kwargs)
+        
+    return wrapper
