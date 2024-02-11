cd81bc4c12851dbefecdfeb32a00c9f101e01ebc
Cole Scheller <schelcc@pobox.com>
2024-02-10 19:34:35 -0500

    drawing manager interface to simplify usage mode differences

---
+++ labelCloud/control/base_drawing_manager.py
@@ -0,0 +1,51 @@
+import logging
+from typing import TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from ..view.gui import GUI
+
+from ..labeling_strategies import BaseLabelingStrategy
+from ..proj_correction_strategies import BaseProjCorrection
+
+from .bbox_controller import BoundingBoxController
+from .projection_controller import ProjectionCorrectionController
+    
+class BaseDrawingManager(object):
+    def __init__(self, drawing_strategy_type) -> None:
+        self.view: "GUI"
+        self.drawing_strategy_type = type(drawing_strategy_type)
+        self.drawing_strategy = None
+        
+    def set_view(self, view: "GUI") -> None:
+        self.view = view
+        self.view.gl_widget.drawing_mode = self
+    
+    def is_active(self) -> bool:
+        return self.drawing_strategy is not None and isinstance(
+            self.drawing_strategy, self.drawing_strategy_type
+        )    
+        
+    def has_preview(self) -> bool:
+        if self.is_active():
+            return self.drawing_strategy.__class__.PREVIEW
+        return False
+    
+    def set_drawing_strategy(self, strategy) -> None:
+        if self.is_active() and self.drawing_strategy == strategy:
+            self.reset()
+            logging.info("Deactivated drawing!")
+        else:
+            if self.is_active():
+                self.reset()
+                logging.info("Reset previous active drawing mode!")
+                
+            self.drawing_strategy = strategy
+        
+    def draw_preview(self) -> None:
+        if self.drawing_strategy is not None:
+            self.drawing_strategy.draw_preview()
+    
+    def reset(self, points_only : bool = False) -> None:
+        if self.is_active():
+            self.drawing_strategy.reset()
+            
