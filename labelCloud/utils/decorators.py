251cae03a5d7a43f2f56a07b6881ff459e20a758
Cole Scheller <schelcc@pobox.com>
2024-02-20 15:36:34 -0500

    debugging decorator

--- labelCloud/utils/decorators.py
+++ labelCloud/utils/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import logging
 
 from ..control.config_manager import config
 
@@ -24,4 +25,16 @@ def in_projection_only_decorator(func):
         if config.get("FILE", "usage_mode") == "projection":
             return func(**args, **kwargs)
         
+    return wrapper
+
+def logging_debug(func):
+    """
+    Logs information about function call at debug level
+    """
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.debug(f"[Call Dbg] Call {func.__name__}")
+        return func(*args, **kwargs)
+    
     return wrapper
