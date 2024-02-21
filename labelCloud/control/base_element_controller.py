30682e946fa5745b464f0be3d2308aba1ed4b340
Cole Scheller <schelcc@pobox.com>
2024-02-21 11:38:24 -0500

    had weird mutability issues, added accessor to help

--- labelCloud/control/base_element_controller.py
+++ labelCloud/control/base_element_controller.py
@@ -5,6 +5,7 @@ Base class for any subsequent element controllers (bboxes, point pairs, etc.)
 import logging
 from typing import TYPE_CHECKING, List, Optional, Callable
 from functools import wraps
+from ..utils.decorators import logging_debug
 
 from .pcd_manager import PointCloudManager
 from ..definitions import Mode
@@ -30,7 +31,8 @@ def has_active_element_decorator(func):
 
 class BaseElementController(object):
     STD_SCALING = config.getfloat("LABEL", "std_scaling")
-    
+   
+     
     def __init__(self, element_type) -> None:
         self.view : GUI 
         self.pcd_manager : PointCloudManager
@@ -42,11 +44,12 @@ class BaseElementController(object):
 
         logging.debug(f"BaseElementControl instantiated | Type: {self.element_type}")
         
-        
+     
     def has_active_element(self) -> bool:
         """Check if element controller has an active element"""
         return 0 <= self.active_element_id <= len(self.elements)
     
+     
     def get_active_element(self) -> Optional[Element]:
         """Return active element if it exists, otherwise None"""
         if self.has_active_element():
@@ -54,6 +57,12 @@ class BaseElementController(object):
         else:
             return None 
 
+    
+    def get_all_elements(self) -> List[Element]:
+        """Return list of all elements"""
+        return self.elements
+
+     
     def refresh_element_list(self) -> None:
         """Refresh elements w/ PCD update"""
         assert self.pcd_manager is not None, "PCD Controller was never set"
@@ -62,27 +71,34 @@ class BaseElementController(object):
         self.set_elements(pts)
         self.update_all()
 
+     
     def select_relative_element(self, amount : int) -> None:
         """Change element some amount relative to current. Will not proceed if
         there is no active element"""
         if self.has_active_element() and 0 <= self.active_element_id + amount < len(self.elements):
             self.active_element_id += amount
 
+     
     def set_view(self, view: "GUI") -> None:
         """Set element controller's gui"""
         self.view = view
 
+     
     def set_pcd_manager(self, pcd_manager : PointCloudManager) -> None:
         """Set element controller's pcd manager"""
         self.pcd_manager = pcd_manager
 
+     
     def update_element(self, element_id: int, element: Element) -> None:
         """Update controller's element at index element_id w/ what's given in "element\".
         Ensures consistency with intial element type"""
         if isinstance(element, self.element_type) and (0 <= element_id < len(self.elements)):
             self.elements[element_id] = element
             self.update_all()
+        else:
+            logging.warning("Element change failed!")
     
+     
     def delete_element(self, element_id : int) -> None:
         """Delete element at index element_id"""
         logging.debug(f"element controller - delete element at index {element_id}")
@@ -91,34 +107,42 @@ class BaseElementController(object):
             if self.active_element_id == element_id:
                 self.set_active_element(len(self.elements) - 1)
                 
+     
     def delete_current_element(self) -> None:
         """Delete active element"""
         self.delete_element(self.active_element_id)
     
+     
     def deselect_element(self) -> None:
         """Deselect active element"""
         self.active_element_id = -1
         self.update_all()
         self.view.status_manager.set_mode(Mode.NAVIGATION)
         
+     
     def set_active_element(self, element_id : int) -> None:
         """Change active element, unsets active element if index outside of range is requested"""
+        logging.debug(f"Change active element from {self.active_element_id} to {element_id}")
         if 0 <= element_id < len(self.elements):
             self.active_element_id = element_id
             self.update_all()
         else:
+            logging.debug(f"Could not set active element to {element_id}")
             self.deselect_element()
             
+     
     def register_add_element_callback(self, callback : Callable) -> None:
         """Add an action to be performed whenever an element is added to the list.
         Input functions shouldn't depend on return behavior."""
         self.add_element_callbacks.append(callback) 
 
+     
     def register_update_active_callback(self, callback : Callable) -> None:
         """Add an action to be performed whenever the active element is changed.
         Input functions shouldn't depend on return behavior."""
         self.update_active_callbacks.append(callback)
 
+     
     def add_element(self, element : Element) -> None:
         logging.debug("ElementController recieved element add:")
         logging.debug(f"Expected type: {self.element_type}\n\t- Received type: {type(element)}")
@@ -133,6 +157,7 @@ class BaseElementController(object):
                 func()
             
             
+     
     def set_elements(self, elements: List[Element]) -> None:
         """Sets controller's element list. Checks if every contained element is consistent w/
         initial type"""
@@ -142,11 +167,13 @@ class BaseElementController(object):
         if len(self.elements) > 0:
             self.set_active_element(len(self.elements)-1)
     
+     
     def reset(self) -> None:
         """Deselect active element and clear current element list"""
         self.deselect_element()
         self.set_elements([]) 
     
+     
     def update_all(self) -> None:
         """Update various UI elements associated with the controller"""
         raise NotImplementedError 
